b05b140536f917a88cc5c860a0d247f7e04ff046
yvonnkr <yvonnkr86@gmail.com>
2024-05-28 12:21:18 +0100

    Initial Commit

---
+++ src/main/java/com/yvolabs/projectmanagementsystemserver/ProjectManagementSystemServerApplication.java
@@ -0,0 +1,13 @@
+package com.yvolabs.projectmanagementsystemserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementSystemServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectManagementSystemServerApplication.class, args);
+    }
+
+}
